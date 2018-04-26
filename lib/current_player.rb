7ec4fb9fa37a4251153c361674ecda20fe37567d
Grace Shih <vocalise@gmail.com>
2018-04-26 04:43:18 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   count = 0
   board.each do |turn|
     if (turn == "X" || turn == "O")
-      count += l
+      count += 1
     end
   end
   return count
@@ -11,7 +11,3 @@ end
 def current_player(count)
   return (count % 2 == 0) ? "X" : "O"
 end
-
-board = ["X", " ", "O"]
-count = turn_count(board)
-puts count
